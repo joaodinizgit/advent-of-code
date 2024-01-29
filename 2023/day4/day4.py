868b6b30fe5203ecef2fbdf28b0854a59fbe25e9
João Diniz <joaodiniz@msn.com>
2024-01-28 22:54:37 -0300

    day 4 part 2

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -3,23 +3,46 @@ import re
 r = "\: | \|"
 s = "[0-9]+"
 total_points = 0
+M = []
+t = 0       # total scratchcards
 
-def c(m):
-    t = 0
-    for i in m[1]:
-        if i in m[0]:
-            t += 1
-    if t > 0:
-        points = 2**(t - 1)
-        return points
-    return 0
+def toM(m):
+    M.append(list(range(m[0] + 1, m[0] + m[1] + 1)))
+
+def scratch(m):
+    def r(m):
+        global t
+        t += len(m)
+        for i in m:
+            if len(M[i - 1]) > 0:
+                r(M[i - 1])
+
+    for i in M:
+        r(i)
+    print(f"Total scratchcards are: {t + len(m)}.")
+
+
+def points(m):
+    w = 0       # w: count winning numbers
+    for i in m[2]:
+        if i in m[1]:
+            w += 1
+    if w > 0:
+        points = 2**(w - 1)
+        return [points, w]
+    return [0, w]
+
 
 with open("day4/input.txt", "r") as f:
     for line in f:
         x = re.split(r, line)
         L = []
-        for i in range(1, len(x)):
+        for i in range(len(x)):
             L.append(re.findall(s, x[i]))
-        total_points += c(L)
+        p = points(L)
+        total_points += p[0]                # cal total points. Part 1
+        toM([int(L[0][0]), p[1]])           # make a copy of cards to M[]
 
-print(total_points)
+
+print(f"Total points: {total_points}.")
+scratch(M)          # calc total scratchs cards. Part 2
