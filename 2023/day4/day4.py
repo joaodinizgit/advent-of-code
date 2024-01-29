8cf87adfad2f05eaaec56703379e4afe76aad8bf
João Diniz <joaodiniz@msn.com>
2024-01-29 12:39:29 -0300

    rearrange code day4

--- 2023/day4/day4.py
+++ 2023/day4/day4.py
@@ -1,15 +1,42 @@
 import re
 
-r = "\: | \|"
-s = "[0-9]+"
-total_points = 0
 M = []
-t = 0       # total scratchcards
+t = 0       # Total number of scratchcards.
 
-def toM(m):
+def main():
+    r = "\: | \|"
+    s = "[0-9]+"
+    total_points = 0
+    with open("day4/input.txt", "r") as f:
+        for line in f:
+            x = re.split(r, line)
+            L = []
+            for i in range(len(x)):
+                L.append(re.findall(s, x[i]))
+            p = points(L)
+            total_points += p[0]                # Calc total points. Part 1.
+            toM([int(L[0][0]), p[1]])           # Make a copy of cards to M[].
+
+    print(f"Total points: {total_points}.")
+    scratch(M)          # To calc total scratchcards. Part 2.
+
+
+def points(m):          # Part 1.
+    w = 0               # w: Count winning numbers.
+    for i in m[2]:
+        if i in m[1]:
+            w += 1
+    if w > 0:
+        points = 2**(w - 1)
+        return [points, w]
+    return [0, w]
+
+
+def toM(m):             # Copy to M[].
     M.append(list(range(m[0] + 1, m[0] + m[1] + 1)))
 
-def scratch(m):
+
+def scratch(m):         # Part 2.
     def r(m):
         global t
         t += len(m)
@@ -22,27 +49,4 @@ def scratch(m):
     print(f"Total scratchcards are: {t + len(m)}.")
 
 
-def points(m):
-    w = 0       # w: count winning numbers
-    for i in m[2]:
-        if i in m[1]:
-            w += 1
-    if w > 0:
-        points = 2**(w - 1)
-        return [points, w]
-    return [0, w]
-
-
-with open("day4/input.txt", "r") as f:
-    for line in f:
-        x = re.split(r, line)
-        L = []
-        for i in range(len(x)):
-            L.append(re.findall(s, x[i]))
-        p = points(L)
-        total_points += p[0]                # cal total points. Part 1
-        toM([int(L[0][0]), p[1]])           # make a copy of cards to M[]
-
-
-print(f"Total points: {total_points}.")
-scratch(M)          # calc total scratchs cards. Part 2
+main()
