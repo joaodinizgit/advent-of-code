89364fb1630a6d4ad1c10e0aec013a90219d5a08
João Diniz <joaodiniz@msn.com>
2024-04-01 22:51:13 -0300

    day14.c p2 done!

--- 2023/day14/day14.c
+++ 2023/day14/day14.c
@@ -7,8 +7,8 @@
 #include <stdlib.h>
 
 enum {
-    MAXCOLS = 200,
-    MAXROWS = 200,
+    MAXCOLS = 110,
+    MAXROWS = 110,
     MAXCYCLES = 300,
     LINEBUF = 10010
 };
@@ -16,7 +16,14 @@ enum {
 void mvnorth(char (*platform)[MAXCOLS], int x, int y);
 void mvwest(char (*platform)[MAXCOLS], int x, int y);
 void mvsouth(char (*platform)[MAXCOLS], int x, int y, int row);
-void mveast(char (*platform)[MAXCOLS], int x, int y, int row);
+void mveast(char (*platform)[MAXCOLS], int x, int y, int lenline);
+
+struct repbuf{
+    char line[LINEBUF];
+    int repn;
+    int tnorth;
+    int lastcycle;
+}repbuf[200];
 
 
 int main(void)
@@ -40,24 +47,15 @@ int main(void)
 
     fclose(f);
     int lenline = strlen(platform[0]);
-    int j = 0;
-    // Scan through rows looking for "O".
+    int i = 0, j = 0;
     int repc = 0;
     int flag_found = 0;
 
-    struct repbuf {
-        char line[LINEBUF];
-        int repn;
-        int tnorth;
-        int lastcycle;
-    }repbuf[200];
-
     // Start cycle.
-    int linerecn = 0;
     int cycle = 0;
     for (cycle = 0; cycle < MAXCYCLES; cycle++) {
         // Move to north.
-        for (int i = 0; i < row; i++) {
+        for (i = 0; i < row; i++) {
             for (j = 0; j < lenline; j++) {
                 if (platform[i][j] == 'O') {
                     // Up the O until # or first row.
@@ -67,7 +65,7 @@ int main(void)
         }
 
         // Move to west.
-        for (int i = 0; i < row; i++) {
+        for (i = 0; i < row; i++) {
             for (j = 0; j < lenline; j++) {
                 if (platform[i][j] == 'O') {
                     mvwest(&platform[0], i, j);
@@ -76,7 +74,7 @@ int main(void)
         }
 
         // Move to south.
-        for (int i = row; i >= 0; i--) {
+        for (i = row - 1; i >= 0; i--) {
             for (j = 0; j < lenline; j++) {
                 if (platform[i][j] == 'O') {
                     mvsouth(&platform[0], i, j, row);
@@ -85,23 +83,23 @@ int main(void)
         }
 
         // Move to east.
-        for (int i = 0; i < row; i++) {
+        for (i = 0; i < row; i++) {
             for (j = lenline - 1; j >= 0; j--) {
                 if (platform[i][j] == 'O') {
-                    mveast(&platform[0], i, j, row);
+                    mveast(&platform[0], i, j, lenline);
                 }
             }
         }
 
         // Store all rows platform in one line to look for repetition.
         char buftocmp[LINEBUF] = {};
-        for (int i = 0; i < row; i++) {
+        for (i = 0; i < row; i++) {
             strcat(buftocmp, platform[i]);
         }
 
         // Check if line already stored.
         flag_found = 0;
-        for (int i = 0; i < repc; i++) {
+        for (i = 0; i < repc; i++) {
             if (strcmp(repbuf[i].line, buftocmp) == 0) {
                 repbuf[i].repn++;
                 flag_found = 1;
@@ -113,8 +111,9 @@ int main(void)
         // If not, store it.
         if (flag_found == 0) {
             strcpy(repbuf[repc].line, buftocmp);
-            linerecn++;
+            // linerecn++;
             repbuf[repc].lastcycle = cycle;
+            repc++;
         }
 
         int totalp1 = 0;
@@ -122,7 +121,7 @@ int main(void)
 
         // North
         // Count O per row and multiply by rown number.
-        for (int i = 0; i < row; i++) {
+        for (i = 0; i < row; i++) {
             for (j = 0; j < lenline; j++) {
                 if (platform[i][j] == 'O') {
                     obyline++;
@@ -133,26 +132,23 @@ int main(void)
         }
 
         if (flag_found == 0) {
-            repbuf[repc].tnorth = totalp1;
+            repbuf[repc - 1].tnorth = totalp1;
         }
-
-        repc++;
     }
 
     int counter = 0;
     // Calculate interval of repetition.
-    for (int i = 0; i < linerecn; i++) {
+    for (int i = 0; i < repc; i++) {
         if (repbuf[i].repn > 0) {
             ++counter;
         }
     }
 
-    for (int i = 0; i < linerecn; i++) {
+    for (int i = 0; i < repc; i++) {
         if (repbuf[i].repn > 0) {
             int eqbi = (1000000000 - (repbuf[i].lastcycle + 1)) % counter;
             if (eqbi == 0) {
                 printf("Answer P2: %i\n", repbuf[i].tnorth);
-                // break;
                 return 0;
             }
         }
@@ -163,7 +159,7 @@ int main(void)
 
 void mvnorth(char (*platform)[MAXCOLS], int x, int y)
 {
-    if (x <= 0 || platform[x - 1][y] == '#' || platform[x - 1][y] == 'O') {
+    if (x <= 0)  {
         return;
     }
 
@@ -177,7 +173,7 @@ void mvnorth(char (*platform)[MAXCOLS], int x, int y)
 
 void mvwest(char (*platform)[MAXCOLS], int x, int y)
 {
-    if (y <= 0 || platform[x][y - 1] == '#' || platform[x][y - 1] == 'O') {
+    if (y <= 0) {
         return;
     }
 
@@ -191,7 +187,7 @@ void mvwest(char (*platform)[MAXCOLS], int x, int y)
 
 void mvsouth(char (*platform)[MAXCOLS], int x, int y, int row)
 {
-    if (x > row || platform[x + 1][y] == '#' || platform[x + 1][y] == 'O') {
+    if (x >= row - 1) {
         return;
     }
 
@@ -200,20 +196,19 @@ void mvsouth(char (*platform)[MAXCOLS], int x, int y, int row)
         platform[x][y] = '.';
         return mvsouth(platform, x + 1, y, row);
     }
-
 }
 
 
-void mveast(char (*platform)[MAXCOLS], int x, int y, int row)
+void mveast(char (*platform)[MAXCOLS], int x, int y, int lenline)
 {
-    if (x > row || platform[x][y + 1] == '#' || platform[x][y + 1] == 'O') {
+    if (y >= lenline - 1){
         return;
     }
 
     if (platform[x][y + 1] == '.') {
         platform[x][y + 1] = 'O';
         platform[x][y] = '.';
-        return mveast(platform, x, y + 1, row);
+        return mveast(platform, x, y + 1, lenline);
     }
 }
 // Answer Part 1: 109755, Part 2: 90928.
