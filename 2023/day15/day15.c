6e4d5bab63be6760ebcb8ddc2c5045eadd9cf203
João Diniz <joaodiniz@msn.com>
2024-04-04 20:32:22 -0300

    day15.c Part 2 DONE!

--- 2023/day15/day15.c
+++ 2023/day15/day15.c
@@ -1,15 +1,18 @@
-/*  Advent of Code 2023 Day 15 Part 1.
+/*  Advent of Code 2023 Day 15 Part 1 and Part 2.
     Author: João Diniz.
     joaodiniz@msn.com
+    Note: For this puzzle I created a Hashmap
+    with the boxes, inside the box the lenses were stored
+    in a Linked list.
 */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 enum {
-    MAXCHARS = 30000,
+    MAXCHARS = 23000,
     ASCII0 = 48,
-    TOTALBOXES = 256,
+    TOTALBOXES = 256
 };
 
 struct node{
@@ -20,15 +23,9 @@ struct node{
 
 struct node *box[TOTALBOXES] = {};
 
-
-
 int hash(char *word);
 void addup(const char *step);
 void sub(const char *step);
-void showmaps(void);
-
-unsigned int sfpower = 0;
-
 
 int main(void)
 {
@@ -38,60 +35,42 @@ int main(void)
         return(-1);
     }
 
-    // Part 1
+    // Part 1.
     char linebuf[MAXCHARS];
     char linebuf2[MAXCHARS];
     int totalsum = 0;
 
     while(fgets(linebuf, MAXCHARS, f) != NULL) {
-        // printf("%s\n", linebuf);
         strcpy(linebuf2, linebuf); // For part 2.
         char *p = strtok(linebuf, ",");
-        // printf("%s\n", p);
         while(p != NULL) {
-            // printf("%s\n", p);
             totalsum += hash(p);
             p = strtok(NULL, ",");
         }
     }
 
-    // strcpy(linebuf2, linebuf);
-    printf("%i\n", totalsum);
+    printf("Answer Part 1: %i\n", totalsum);
     fclose(f);
 
-
-    // Part 2
-    printf("SECOND PART\n");
-    // printf("%s\n", linebuf2);
+    // Part 2.
+    int sfpower = 0;
     char *p = strtok(linebuf2, ",");
-    printf("%s\n", p);
     while(p != NULL) {
-        // printf("%s %i ", p, strlen(p));
         int lenp = strlen(p);
-
-        // Find operation
+        // Find operation.
         int i = 0;
         for (i = 0; i < lenp; i++) {
             if (p[i] == '='){
-                // printf("Add or update\n");
-                // printf("%s\n", p);
                 addup(p);
-                // showmaps();
                 break;
-
             } else if (p[i] == '-') {
-                // printf("Found - Sub\n");
-                // printf("%s\n", p);
-                // Subtract from a linked list.
                 sub(p);
-                // showmaps();
                 break;
             }
         }
         p = strtok(NULL, ",");
     }
 
-    printf("LIST BOXS\n");
     struct node *ptr2 = NULL;
     int i = 0;
     int slotposition = 0;
@@ -99,7 +78,7 @@ int main(void)
         slotposition = 0;
         ptr2 = box[i];
         while(ptr2 != NULL) {
-            printf("[%s %i BOX %i] ", ptr2->label, ptr2->focal, i);
+            // printf("[%s %i BOX %i] ", ptr2->label, ptr2->focal, i);
             slotposition++;
             sfpower += ((i + 1) * ((slotposition) * (ptr2->focal)));
             ptr2 = ptr2->next;
@@ -108,24 +87,31 @@ int main(void)
 
     printf("Answer Part 2: %i\n", sfpower);
 
+    // Clear memory to avoid leaks.
+    struct node *ptr3 = NULL;
+    for (i = 0; i < TOTALBOXES; i++) {
+        ptr2 = box[i];
+        while(ptr2 != NULL) {
+            ptr3 = ptr2->next;
+            free(ptr2);
+            ptr2 = ptr3;
+        }
+    }
 }
 
 
 int hash(char *word)
 {
-    // printf("%s\n", word);
+    int i = 0;
     int curvalue = 0;
     int lenw = strlen(word);
-    int i = 0;
 
     for(i = 0; i < lenw; i++) {
-        // printf("%c\n", *(word + i));
         curvalue += *(word + i);
         curvalue *= 17;
-        curvalue = curvalue % 256;
-
+        curvalue = curvalue % TOTALBOXES;
     }
-    // printf("%i\n", curvalue);
+
     return curvalue;
 }
 
@@ -134,7 +120,7 @@ void addup(const char *step)
 {
     int i = 0;
     int tmpfocal = 0;
-    char tmplabel[30]={};
+    char tmplabel[20] = {};
     struct node *n = NULL;
 
     while (*(step + i) != '=') {
@@ -142,13 +128,17 @@ void addup(const char *step)
         i++;
     }
     tmpfocal = step[i + 1] - ASCII0;
-    // printf("Len %s and focal %i\n", tmplabel, tmpfocal);
-    // Cal the hash to insert in a specific box from 0 to 255.
+    // Calculate the hash to insert in a specific box from 0 to 255.
     int b = hash(tmplabel);
-    printf("++Add %s %i in BOX: %i\n", tmplabel, tmpfocal, b);
 
-    // Add in Linked list accordin to the Box.
+    // Add in Linked list according to the Box.
     n = malloc(sizeof(node));
+    if (n == NULL) {
+        printf("No memory available");
+        free(n);
+        exit(1);
+    }
+
     strcpy(n->label, tmplabel);
     n->focal = tmpfocal;
     n->next = NULL;
@@ -157,25 +147,28 @@ void addup(const char *step)
         box[b] = n;
     } else {
         for(struct node *ptr = box[b]; ptr != NULL; ptr = ptr->next) {
-            // Update focal length If already exist
+            // Update focal length If lense already exist.
             if (strcmp(ptr->label, tmplabel) == 0) {
                 ptr->focal = tmpfocal;
+                free(n);
                 break;
             }
 
-            // Insert
+            // Insert lense.
             if (ptr->next == NULL) {
                 ptr->next = n;
+                n = NULL;
                 break;
             }
         }
     }
 }
 
+
 void sub(const char *step)
 {
     int i = 0;
-    char tmplabel[20]={};
+    char tmplabel[20] = {};
     struct node *n = NULL;
 
     while (*(step + i) != '-') {
@@ -184,58 +177,26 @@ void sub(const char *step)
     }
 
     int b = hash(tmplabel);
-    printf("--Sub %s from BOX: %i\n", tmplabel, b);
 
     struct node *deltemp = NULL;
 
     for(struct node *ptr = box[b]; ptr != NULL; ptr = ptr->next) {
-        // Test: Trying to exclude number insert are equal
          if (ptr->next != NULL && (strcmp(ptr->next->label, tmplabel) == 0)) {
-        //     printf("The next will be equal to find\n");
-        //     // Exclude node.
             deltemp = ptr->next;
-
             ptr->next = ptr->next->next;
             free(deltemp);
             deltemp = NULL;
             break;
         }
 
-        // // Condition for the first number
-        if (((strcmp(ptr->label, tmplabel) == 0))) {
-            deltemp = ptr;
+        // Condition for the first node.
+        if (strcmp(ptr->label, tmplabel) == 0) {
+            deltemp = box[b];
             box[b] = ptr->next;
-            // printf("Number exist!\n");
-            free(n);
-            n = NULL;
             free(deltemp);
             deltemp = NULL;
             break;
         }
     }
-
-
-    // }
-
-
-
-
-
 }
-
-
-void showmaps(void)
-{
-    struct node *ptr2 = NULL;
-    int i = 0;
-    int z = 0;
-    unsigned int slotposition = 0;
-    for (i = 0; i < 256; i++) {
-        ptr2 = box[i];
-        while(ptr2 != NULL) {
-            printf("[%s %i BOX %i] ", ptr2->label, ptr2->focal, i);
-            ptr2 = ptr2->next;
-        }
-    }
-    printf("\n");
-}
+/* Answer Part 1: 514025. Part 2: 244461.*/
